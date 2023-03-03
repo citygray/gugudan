c5edfecb2b11daf8280a3f219bf8a1e2558e317a
citygray <nanee9244@gamail.com>
2023-03-03 15:55:19 +0900

    "8,7"과 같은 문자열을 입력하면 팔칠단을 구현한다.

--- src/gugudan/GugudanMain.java
+++ src/gugudan/GugudanMain.java
@@ -4,15 +4,20 @@ import java.util.Scanner;
 
 public class GugudanMain {
 	public static void main(String[] args) {
-		//	최종 요구사항 1
+		//	최종 요구사항 2
 		//	사용자가 입력한 값에 따라 크기가 다른 구구단을 계산해 출력한다.
-		//	예를 들어 사용자가 8을 입력하면 팔팔단, 19를 입력하면 십구십구단(2 * 1에서 19 * 19)을 계산해 출력한다.
-		//	이 요구사항은 앞의 실습을 소화했으면 구현할 수 있기 때문에 생략한다.
+		//  예를 들어 사용자가 "8,7"과 같은 문자열을 입력하면 팔칠단을 구현한다. 
+		//	팔칠단은 2 * 1 ... 2 * 7, 3 * 1 ... 3 * 7, ... , 8 * 1 ... 8 * 7 까지 구현하는 것을 의미한다.
 		
-		System.out.println("숫자를 입력해주세요");
+		System.out.println("두개의 숫자를 ','로 구분하여 입력해주세요");
 		Scanner scanner = new Scanner(System.in);
-		int inputNumber = scanner.nextInt();
-		Gugudan.calculate(inputNumber);
+		String inputString = scanner.nextLine();
+		String[] splitedStr = inputString.split(",");
+		
+		int firstInt = Integer.parseInt(splitedStr[0]);
+		int secondInt = Integer.parseInt(splitedStr[1]);
+		
+		Gugudan.calculate(firstInt,secondInt);
 	}
 
 
