91c275dd855f120fcb2c071e117b2714ad3f3861
citygray <nanee9244@gamail.com>
2023-03-03 15:20:06 +0900

    입력값으로 크기가 다른 구구단 만들기 - 8을 입력하면 팔팔단

--- src/gugudan/Gugudan.java
+++ src/gugudan/Gugudan.java
@@ -1,33 +1,26 @@
 package gugudan;
 
+import java.util.Scanner;
 
 public class Gugudan {
 	
-	/*
-		학습 목표
-		- 메소드를 활용해 구구단을 구현하는 경험을 한다.
-		요구사항
-		- 구구단을 구현하기 위해 반복적으로 발생하는 부분이 있다.
-		- 반복적으로 발생하는 부분을 메소드를 활용해 반복되는 부분을 줄인다.
-		힌트
-		- 구구단 계산 결과를 배열에 담는 로직을 새로운 메소드로 분리한다.
-		- 배열에 담은 데이터를 화면에 출력하는 로직을 새로운 메소드로 분리한다.
-	*/
-	//구구단 계산 결과를 배열에 담는 로직을 새로운 메소드로 분리한다.
-	public static int[] calculate(int times) {
-		
-		//for문을 돌면서 구구단 실행 결과를 배열에 저장한다.
-		int[] result = new int[9];
-		for(int i=0; i<result.length; i++) {
-			result[i] = times * (i+1);
+	
+	//	최종 요구사항 1
+	//	사용자가 입력한 값에 따라 크기가 다른 구구단을 계산해 출력한다.
+	//	예를 들어 사용자가 8을 입력하면 팔팔단, 19를 입력하면 십구십구단(2 * 1에서 19 * 19)을 계산해 출력한다.
+	//	이 요구사항은 앞의 실습을 소화했으면 구현할 수 있기 때문에 생략한다.
+	
+	public static void calculate(int inputNumber) {
+		int[] result = new int[inputNumber];
+		for(int i=1; i< result.length; i++) {
+			for(int j=0; j<result.length; j++) {
+				result[j] = (i+1)*(j+1);
+				System.out.println((i+1)+"*"+(j+1)+"="+result[j]);
+			} 
+			System.out.println("------------------------");
+					
 		}
-		
-		return result;
+
 	}
-	//배열에 담은 데이터를 화면에 출력하는 로직을 새로운 메소드로 분리한다.
-	public static void print(int[] result, int j) {
-		for(int i=0; i<result.length; i++) {
-			System.out.println(j+"*"+(i+1)+"="+result[i]);
-		}
-	}
+	
 }
--- src/gugudan/GugudanMain.java
+++ src/gugudan/GugudanMain.java
@@ -1,13 +1,19 @@
 package gugudan;
 
+import java.util.Scanner;
+
 public class GugudanMain {
 	public static void main(String[] args) {
+		//	최종 요구사항 1
+		//	사용자가 입력한 값에 따라 크기가 다른 구구단을 계산해 출력한다.
+		//	예를 들어 사용자가 8을 입력하면 팔팔단, 19를 입력하면 십구십구단(2 * 1에서 19 * 19)을 계산해 출력한다.
+		//	이 요구사항은 앞의 실습을 소화했으면 구현할 수 있기 때문에 생략한다.
 		
-		for(int j=2; j<10; j++) {
-			// calculate와 print 메소드 활용해 구구단 구현하기
-			Gugudan.print(Gugudan.calculate(j), j);
-			System.out.println("-----------------------------");
-		}
+		System.out.println("숫자를 입력해주세요");
+		Scanner scanner = new Scanner(System.in);
+		int inputNumber = scanner.nextInt();
+		Gugudan.calculate(inputNumber);
 	}
+
 
 }
